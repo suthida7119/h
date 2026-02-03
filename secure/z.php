0f91b02f2380205064ec2d154a97b48906c36b03
suthida7119 <66010914085@msu.ac.th>
2026-02-03 19:55:08 +0700

    Add files via upload

--- secure/z.php
+++ secure/z.php
@@ -1,3 +1,8 @@
 <?php
+echo "<h3>รหัสสำหรับ Admin คนที่ 1 (1234)</h3>";
+echo password_hash("1234", PASSWORD_DEFAULT);
+echo "<hr>";
+
+echo "<h3>รหัสสำหรับ Admin คนที่ 2 (4321)</h3>";
 echo password_hash("4321", PASSWORD_DEFAULT);
 ?>
